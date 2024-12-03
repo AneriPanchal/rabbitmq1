b884d6e0382b4b47b664cf0f5e4c44bcffde8338
AneriPanchal <aneAnerianeri@o365.siprtc.io>
2024-12-03 08:30:09 -0700

    updated task3

---
+++ task3/producer/producer.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"time"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"shared_queue", // name
+		false,          // durable
+		false,          // delete when unused
+		false,          // exclusive
+		false,          // no-wait
+		nil,            // arguments
+	)
+	if err != nil {
+		log.Fatalf("Failed to declare a queue: %v", err)
+	}
+
+	for i := 0; i < 10; i++ {
+		body := "Message " + time.Now().Format(time.RFC3339)
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		if err != nil {
+			log.Fatalf("Failed to publish a message: %v", err)
+		}
+		log.Printf("Producer [x] Sent %s", body)
+		time.Sleep(1 * time.Second) // simulate delay
+	}
+}
